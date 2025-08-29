41788663334e8aba9113f45865a497cfbf7759fc
T0bzz <t.vernon1@lancaster.ac.uk>
2025-08-29 18:30:33 +0100

    feat: add flatten helper function

--- helper_functions.py
+++ helper_functions.py
@@ -3,4 +3,16 @@ from torch import nn
 
 # Flatten a tensor except for the first dimension (batch size)
 def flatten(x):
-    return x.reshape(x.size(0), -1) # reshape handles non-contiguous tensors
+    """
+    Flatten a tensor except for the first dimension (batch size)
+    Args:
+        x (torch.Tensor): Input tensor of shape (Batch_size, C, H, W)
+
+    Returns:
+        flattened_size (int): Size of the flattened tensor excluding batch size.
+        flattened_tensor (torch.Tensor): Flattened tensor of shape (Batch_size, flattened_size)
+    """
+
+    flattened_size = x.numel() // x.size(0)
+    flattened_tensor = x.reshape(x.size(0), -1)
+    return flattened_size, flattened_tensor  # reshape handles non-contiguous tensors
